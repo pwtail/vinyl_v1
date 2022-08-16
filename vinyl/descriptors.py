3b945027970c4df81a59047f4975b7792c0105d3
pwtail <abvit89s@gmail.com>
2022-08-16 11:13:35 +0300

    lazy: not needed

--- vinyl/descriptors.py
+++ vinyl/descriptors.py
@@ -1,33 +1,28 @@
 from vinyl import deferred
 
 
-#lazy ??????
-
 class ManyToManyDescriptor:
 
     def __get__(self, instance, owner):
         django_model = owner._model
         wrapped = getattr(django_model, self.name)
         manager = wrapped.__get__(instance, owner)
-        manager.__class__ = wrap_related_manager(manager.__class__)
-        return manager
+        # manager.__class__ = wrap_related_manager(manager.__class__)
+        if wrapper := getattr(manager, 'vinyl_wrapper', None):
+            return wrapper
+        manager.vinyl_wrapper = Wrapper(manager)
+        return manager.vinyl_wrapper
 
 
     def __set_name__(self, owner, name):
         self.name = name
         print('name', name)
 
-#TODO cache
 
-def wrap_related_manager(cls):
-    class RelatedManager(cls):
+class Wrapper:
+    def __init__(self, manager):
+        self.manager = manager
 
-        async def add(self, *objs):
-            async with deferred.driver():
-                print(cls.add.__get__(self))
-                super().add(*objs)
-
-    return RelatedManager
-
-
-
+    async def add(self, *objs):
+        async with deferred.driver():
+            self.manager.add(*objs)
