2a58f970cda8271e669f2088c2679a5fc1d8f99d
pwtail <abvit89s@gmail.com>
2022-09-01 18:04:30 +0300

    builtins.Await

--- vinyl/__init__.py
+++ vinyl/__init__.py
@@ -1,3 +1,6 @@
-
+import builtins
 
 from . import patches
+from .util import Await
+
+builtins.Await = Await
--- vinyl/util.py
+++ vinyl/util.py
@@ -1,9 +1,8 @@
 from django.db.models import QuerySet
 
-from vinyl.queryset import VinylQuerySet
-
 
 def Await(obj):
     if isinstance(obj, QuerySet):
+        from vinyl.queryset import VinylQuerySet
         return VinylQuerySet.__Await__(obj)
     return obj.__Await__()
