67a9b5af93a2ba4cd62f9168455098c7f0294899
pwtail <abvit89s@gmail.com>
2022-09-02 21:23:47 +0300

    test (WIP)

--- transform.sh
+++ transform.sh
@@ -1,6 +1,10 @@
-
+# no backend
 python -m transform vinyl/compiler.py
 python -m transform vinyl/query.py
 python -m transform vinyl/queryset.py
-#python -m vinyl.transform vinyl/compiler.py
-#python -m vinyl.transform vinyl/compiler.py
+python -m transform vinyl/model.py
+python -m transform vinyl/prefetch.py
+python -m transform vinyl/iterables.py
+python -m transform vinyl/insert.py
+python -m transform vinyl/descriptors.py
+python -m transform vinyl/deferred.py
--- vinyl/flags.py
+++ vinyl/flags.py
@@ -1,4 +1,9 @@
-IS_ASYNC = True
+try:
+    import asyncflag
+except ImportError:
+    IS_ASYNC = False
+else:
+    IS_ASYNC = True
 
 
 def is_async():
